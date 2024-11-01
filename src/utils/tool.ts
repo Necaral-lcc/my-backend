a440d0a6e4b467ab8fce47c75398693d1a0e0513
Necaral <770162890@qq.com>
2024-11-01 13:49:22 +0800

    添加方法说明

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -1,23 +1,30 @@
 interface sList {
   id: number
   parentId: number | null
-  [key: string]: any
 }
 
-interface sTree extends sList {
-  children: sTree[]
+type ITree<T extends Object> = T & {
+  children: ITree<T>[]
 }
-
-export const listToTree = (ar: sList[], parentId: number | null) => {
+/**
+ * 将列表转化为树结构
+ * @param ar  列表
+ * @param parentId   父节点id
+ * @returns 树结构
+ */
+export const listToTree = <T extends sList>(
+  ar: T[],
+  parentId: number | null
+) => {
   return ar
     .filter((item) => item.parentId === parentId)
     .map((item) => {
-      const obj: sTree = {
-        ...item,
-        parentId: item.parentId || 0,
+      const obj: ITree<T> = {
+        ...item, // 继承父节点的属性
+        parentId: item.parentId || 0, // 根节点的parentId为null，需要转换为0
         children: listToTree(
-          ar.filter((i) => i.parentId !== parentId),
-          item.id
+          ar.filter((i) => i.parentId !== parentId), // 过滤掉父节点的子节点
+          item.id // 父节点的id作为子节点的父节点id
         ),
       }
       return obj
